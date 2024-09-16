59b678eaee83588fa42ef25e462ac77a7ac9bc73
Rino Naito <rnaito@c3r4s3.42tokyo.jp>
2024-09-16 13:52:17 +0900

    MOD: change bufffer size to 512

--- includes/event_handler.h
+++ includes/event_handler.h
@@ -41,7 +41,7 @@ class EventHandler{
 		int	listening_socket_;
 		sockaddr_in	server_address_;
 		static const int	kQueueLimit;
-		static const int	kBufferSize = 510;
+		static const int	kBufferSize = 512;
 };
 
 #endif
