df96465caca3a0e58e5b7f5aee6c760d04025139
Rino Naito <rnaito@c5r3s13.42tokyo.jp>
2024-09-22 19:02:59 +0900

    UPDATE: just before leaving from school

--- includes/event_handler.h
+++ includes/event_handler.h
@@ -41,7 +41,7 @@ class EventHandler{
 		StartEventListener* start_event_listener_;
 		EndEventListener* end_event_listener_;
 		std::vector<struct pollfd>	poll_fd_;
-		std::map<int, std::string>	response_map_;
+		std::multimap<int, std::string>	response_map_;
 		int	listening_socket_;
 		sockaddr_in	server_address_;
 
