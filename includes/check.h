df19b1f2f140f74b520030004f1708f0b1b00f57
RinoNaito <rinodelevingne@gmail.com>
2024-09-25 19:58:42 +0900

    ADD: add chech/channel class

--- includes/check.h
+++ includes/check.h
@@ -1,14 +1,12 @@
-#ifndef CHANNEL_H_
- # define CHANNEL_H_
+#ifndef CHECK_H_
+ # define CHECK_H_
 
 #include "prepare_event_listener.h"
 
-class User;
-
-class Channel: public PrepareEventListener{
+class Check: public PrepareEventListener{
 	public:
-		Channel();
-		~Channel();
+		Check();
+		~Check();
 		std::map<int, std::string> PassCommand(Event& event);
 		std::map<int, std::string> NickCommand(Event& event);
 		std::map<int, std::string> UserCommand(Event& event);
@@ -20,17 +18,6 @@ class Channel: public PrepareEventListener{
 		std::map<int, std::string> PrivmsgCommand(Event& event);
 
 	private:
-		std::vector<User>	users_;
-		std::string		topic_;
-		std::string		key_;
-		std::string		name_;
-		std::vector<User>	operators_;
-		int	max_member_num_;
-		bool	i_mode;
-		bool	t_mode;
-		bool	k_mode;
-		bool	o_mode;
-		bool	l_mode;
 };
 
 #endif
---
+++ src/check.cpp
@@ -0,0 +1,54 @@
+#include "check.h"
+
+Check::Check(){
+}
+
+Check::~Check(){
+
+}
+
+std::map<int, std::string> Check::PassCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::NickCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::UserCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::JoinCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::InviteCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::KickCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::TopicCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::ModeCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
+std::map<int, std::string> Check::PrivmsgCommand(Event& event){
+	(void)event;
+	std::map<int, std::string> error_message;
+	return error_message;
+}
