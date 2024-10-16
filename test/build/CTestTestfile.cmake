b60a4822512cc3591c4486e7e8fc3a19cf6e20f2
Weijuan Chen <68696117+weijuan82113@users.noreply.github.com>
2024-10-16 12:20:31 +0900

    ADD add GoogleTest CMake

---
+++ test/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/weijuan/projects/42Tokyo/IRC/ft_irc/ft_irc/test
+# Build directory: /Users/weijuan/projects/42Tokyo/IRC/ft_irc/ft_irc/test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/Users/weijuan/projects/42Tokyo/IRC/ft_irc/ft_irc/test/build/hello_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ test/build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/weijuan/projects/42Tokyo/IRC/ft_irc/ft_irc/test/build/_deps/googletest-src/googletest
+# Build directory: /Users/weijuan/projects/42Tokyo/IRC/ft_irc/ft_irc/test/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
