936dbebd6e06073407c30877218cea91a5472d19
kitsuki <IT.King5963@gmail.com>
2024-10-24 00:26:43 +0900

    databaseにcheckを追加＆書き方修正

--- src/database.cpp
+++ src/database.cpp
@@ -1,29 +1,26 @@
 #include "database.h"
-
+#include "message.h"
 
 Database::Database(){};
 Database::~Database(){};
 
-void Database::CreateUser(int fd)
-{
-	try
-	{
+void Database::CreateUser(int fd) {
+	try {
 		User* user = new User(fd);
 		check_element_.push_back(user);
 		execute_element_.push_back(user);
-	}
-	catch(const std::bad_alloc& e)
-	{
+	} catch(const std::bad_alloc& e) {
 		std::cerr << "bad to alloc memory" << e.what() << '\n';
 	}
 }
 
 void Database::CheckEvent(Event& event) const {
+	Database::CheckCommandAndParams(event);
+	if (event.HasErrorOccurred())
+		return ;
 	std::size_t vector_length = check_element_.size();
 	for (std::size_t i = 0; i < vector_length; i++)
-	{
 		check_element_[i] -> CheckCommand(event);
-	}
 }
 
 std::map<int, std::string>	Database::ExecuteEvent(const Event& event) {
@@ -37,7 +34,7 @@ std::map<int, std::string>	Database::ExecuteEvent(const Event& event) {
 	return ret;
 }
 
-void	Database::DeleteFinishedElements() {
+void Database::DeleteFinishedElements() {
 	std::set<Finishable *> ptr_set;
 
 	Database::EraseAndAdd(check_element_, ptr_set);
@@ -46,3 +43,93 @@ void	Database::DeleteFinishedElements() {
 	for (std::set<Finishable *>::iterator it = ptr_set.begin(); it != ptr_set.end(); ++it)
 		delete *it;
 }
+
+void Database::CheckCommandAndParams(Event& event) const {
+	switch (event.get_command()) {
+		case message::kPass:
+			CkPassCommand(event);
+			break ;
+		case message::kNick:
+			CkNickCommand(event);
+			break ;
+		case message::kUser:
+			CkUserCommand(event);
+			break ;
+		case message::kJoin:
+			CkJoinCommand(event);
+			break ;
+		case message::kInvite:
+			CkInviteCommand(event);
+			break ;
+		case message::kKick:
+			CkKickCommand(event);
+			break ;
+		case message::kTopic:
+			CkTopicCommand(event);
+			break ;
+		case message::kMode:
+			CkModeCommand(event);
+			break ;
+		case message::kPrivmsg:
+			CkPrivmsgCommand(event);
+			break ;
+		default:
+			break ;
+	}
+}
+
+//Check
+void Database::CkPassCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check Pass called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkNickCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check Nick called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkUserCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check User called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkJoinCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check Join called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkInviteCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check vite called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkKickCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check Kick called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkTopicCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check opic called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkPrivmsgCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check vmsg called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+
+void Database::CkModeCommand(Event& event) const {
+	(void)event;
+	std::cout << "Check Mode called!" << std::endl;
+	utils::PrintStringVector(event.get_command_params());
+}
+//check
